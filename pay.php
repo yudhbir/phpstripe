7f66669c4a67c2c385f3468d0b09a378f0335a46
yudhbir <sachinsingh3066@gmail.com>
2016-12-19 17:24:09 +0530

    first commit

--- pay.php
+++ pay.php
@@ -1,6 +1,6 @@
 <?php
 require_once('vendor/autoload.php');
-\Stripe\Stripe::setApiKey('sk_test_DrN3vT1Ywq4ZHCbCycVBsgWG');
+\Stripe\Stripe::setApiKey('xxxxxxxxxxxxxxxxxxxxxxxx');
 /**
 *
 * Make a charge payment
